250a1d06200d469d424b6b564385c612914715e7
rotemOvadia <>
2018-02-26 23:21:44 +0200

    style changes

--- src/app/features/recipe-description/recipe-description.component.ts
+++ src/app/features/recipe-description/recipe-description.component.ts
@@ -3,7 +3,22 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'recipe-description',
   templateUrl: './recipe-description.component.html',
-  styles: [`#my-logout-button { background: #F44336 }`]
+  styles: [`
+    .description-wrapper {
+      border: 1px solid black;
+      padding: 15px;
+      direction: rtl;
+      margin-right: 300px;
+      margin-top: 30px;
+    }
+    .title-wrapper {
+      color: palevioletred;
+      margin: 3px;
+    }
+    .content-wrapper {
+      color: blue;
+    }
+  `]
 })
 
 export class RecipeDescriptionComponent {
