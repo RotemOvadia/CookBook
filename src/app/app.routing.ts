23c6a0e05aa795008fb3d549b65c25aca111c934
rotemo <rotemo@algotec.co.il>
2018-03-12 12:12:28 +0200

    adding some routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,4 +1,10 @@
 /* tslint:disable: max-line-length */
 import { Routes } from '@angular/router';
+import {DashboardComponent} from './features/dashboard.component';
+import {RecipeComponent} from './features/recipe/recipe.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'recipes', component: DashboardComponent},
+  {path: 'recipes/:id', component: RecipeComponent},
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'}
+];
